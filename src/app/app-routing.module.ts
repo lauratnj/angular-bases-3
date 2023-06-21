083c73e870c3d88c2509d85e8315afd714af208c
Laura Picon Espegel <laura_picon@innotecsystem.com>
2023-06-21 16:50:27 +0200

    esructura de páginas y searchbox component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { AboutPageComponent } from './shared/pages/about-page/about-page.compone
 import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 
+// LazyLoad: Permite de manera perezosa cargar un módulo. Esto significa cargarlo bajo demanda (cuando el usuario lo solicita) y luego queda en memoria. Para ello en vez de componente hemos puesto = loadChildren: () => import('./countries/countries.module').then( module => module.CountriesModule )  --> puedes poner module o m
+
 const routes: Routes = [
-  {
+/*   {
     path:'',
     component: HomePageComponent
-  },
+  }, */
   {
     path:'about',
     component: AboutPageComponent
@@ -19,9 +21,13 @@ const routes: Routes = [
     component: ContactPageComponent
   },
   {
+    path:'countries',
+    loadChildren: () => import('./countries/countries.module').then( m => m.CountriesModule )
+  },
+  {
     path:'**',
-    redirectTo: ''
-    },
+    redirectTo: 'countries'
+  },
 ]
 
 //forRoot lo pone porque es el enrutado principal, cuando sean enrutados secundarios de la aplicación pondrá forChild
