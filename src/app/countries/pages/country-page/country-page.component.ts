f5288011d3718a703d1488dde17bff0a08c060ce
Laura Picon Espegel <laura_picon@innotecsystem.com>
2023-06-28 09:02:13 +0200

    switchMap

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+
 
 @Component({
   selector: 'countries-country-page',
@@ -12,18 +14,31 @@ export class CountryPageComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
+    private router: Router,
     private countriesService: CountriesService,
     ) {}
 
   ngOnInit(): void {
+
     this.activatedRoute.params
-      .subscribe( ({ id }) => {
+      .pipe(
+        switchMap (({ id }) => this.countriesService.searchCountryByAlphaCode( id ) ),
+      )
+      .subscribe( country => {
 
-        this.countriesService.searchCountryByAlphaCode( id )
-          .subscribe( country => {
-            console.log({ country})
-          });
+        if ( !country ) {
+          return this.router.navigateByUrl('');
+        }
 
+        console.log('TENEMOS UN PAÍS');
+        return;
       });
   }
+
+/*   searchCountry( params: Params ) {
+    this.countriesService.searchCountryByAlphaCode ( params['id'] )
+      .subscribe( country => {
+        console.log({ country })
+      });
+  } */
 }
