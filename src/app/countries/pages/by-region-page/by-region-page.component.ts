9bbe3440477a420750fb5089d1c22cff9c0dece9
Laura Picon Espegel <laura_picon@innotecsystem.com>
2023-07-03 15:28:43 +0200

    Optimizar buscar por region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,10 +12,15 @@ import { Country } from '../../interfaces/country';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor( private countriesService: CountriesService ){}
 
-  searchByRegion( region: string):void {
+  searchByRegion( region: Region):void {
+
+    this.selectedRegion = region;
+
     this.countriesService.searchRegion( region ).subscribe ( countries => {
       this.countries = countries;
     });
